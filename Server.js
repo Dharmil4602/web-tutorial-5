494e42073b5273690718bb86d3663dd1ae0ecb96
Dharmil Shah <dharmilshah476@gmail.com>
2024-03-11 21:14:20 -0300

    added error handling

--- Server.js
+++ Server.js
@@ -22,42 +22,49 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/add", (req, res) => {
-    const { email, firstName } = req.body;
-    if (!firstName || !email) {
-        res.status(400).json({message: "Please enter valid data", success: false});
-    } else {
-        const newUser = {
-            id: `${Math.random().toString(36).slice(2)}`,
-            firstName,
-            email
-        };
-        sampleUserData.push(newUser);
-        res.json({message: "User added", success: true});
-    }
-})
+  const { email, firstName } = req.body;
+  if (!firstName || !email) {
+    res
+      .status(400)
+      .json({ message: "Please enter valid data", success: false });
+  } else {
+    const newUser = {
+      id: `${Math.random().toString(36).slice(2)}`,
+      firstName,
+      email,
+    };
+    sampleUserData.push(newUser);
+    res.json({ message: "User added", success: true });
+  }
+});
 
-app.put('/update/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, email } = req.body;
-    const user = sampleUserData.find(user => user.id === id);
-    if (!user) {
-        res.status(404).json({message: "User not found", success: false});
-    } else {
-        user.firstName = firstName;
-        user.email = email;
-        res.json({message: "User updated", success: true});
-    }
-})
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, email } = req.body;
+  const user = sampleUserData.find((user) => user.id === id);
+  if (!user) {
+    res.status(404).json({ message: "User not found", success: false });
+  } else {
+    user.firstName = firstName;
+    user.email = email;
+    res.json({ message: "User updated", success: true });
+  }
+});
 
-app.get('/user/:id', (req, res) => {
-    const { id } = req.params;
-    const user = sampleUserData.find(user => user.id === id);
-    if (!user) {
-        res.status(404).json({message: "User not found", success: false});
-    } else {
-        res.json({success: true, user});
-    }
-})
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const user = sampleUserData.find((user) => user.id === id);
+  if (!user) {
+    res.status(404).json({ message: "User not found", success: false });
+  } else {
+    res.json({ success: true, user });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error", success: false });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
